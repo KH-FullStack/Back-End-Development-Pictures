0a03c0e1ad3580a3ffbbe388fd16da78d641c703
KH-FullStack <picfork@protonmail.com>
2025-05-15 04:57:40 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,6 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
+    return jsonify(data), 200
     pass
 
 ######################################################################
@@ -44,16 +45,33 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
-
+    for picture in data:
+        if picture["id"] == id:
+            return jsonify(picture), 200
+    return jsonify({"error": "Picture not found"}), 404
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
-@app.route("/picture", methods=["POST"])
-def create_picture():
-    pass
+# @app.route("/picture", methods=["POST"])
+# def create_picture():
+#     pass
 
+
+@app.route("/picture/<int:id>", methods=["POST"])
+def create_picture():
+    picture = request.get_json()
+    
+    # Check if the picture with the same id already exists
+    if any(p['id'] == picture['id'] for p in data):
+        return jsonify({"Message": f"picture with id {picture['id']} already present"}), 302
+    data.append(picture)
+    return jsonify(picture), 201
+
+
+@app.route("/count", methods=["GET"])
+def get_count():
+    return jsonify({"length": len(data)}), 200
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
@@ -61,11 +79,24 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    picture = request.get_json()
+    for i, p in enumerate(data):
+        if p["id"] == id:
+            data[i] = picture
+            with open(json_url, "w") as json_file:
+                json.dump(data, json_file)
+            return jsonify(picture), 200
+    return jsonify({"error": "Picture not found"}), 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for i, p in enumerate(data):
+        if p["id"] == id:
+            del data[i]
+            with open(json_url, "w") as json_file:
+                json.dump(data, json_file)
+            return jsonify({"message": "Picture deleted"}), 200
+    return jsonify({"error": "Picture not found"}), 404
